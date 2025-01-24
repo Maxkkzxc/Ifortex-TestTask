5ae4061668ae639705ed5199fc2c7f904155285e
Maxim <maxkkzx@gmail.com>
2025-01-24 20:26:09 +0300

    Task gone

---
+++ TestTask/Services/Implementations/AuthorService.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class AuthorService : IAuthorService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AuthorService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Author> GetAuthor()
+        {
+            var result = await _context.Books
+                .OrderByDescending(b => b.Title.Length)
+                .ThenBy(b => b.AuthorId)
+                .Select(b => new { b.AuthorId })
+                .FirstOrDefaultAsync();
+
+            return await _context.Authors
+                .FirstOrDefaultAsync(a => a.Id == result.AuthorId);
+        }
+
+        public async Task<List<Author>> GetAuthors()
+        {
+            var authorsWithEvenBooksAfter2015 = await _context.Authors
+               .Where(author => author.Books
+               .Count(book => book.PublishDate.Year > 2015) % 2 == 0)
+               .Where(author => author.Books.Any(book => book.PublishDate.Year > 2015))
+               .ToListAsync();
+
+            return authorsWithEvenBooksAfter2015;
+        }
+    }
+}
---
+++ TestTask/Services/Implementations/BookService.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class BookService : IBookService
+    {
+        private readonly ApplicationDbContext _context;
+        private static readonly DateTime CarolusRexSabatonDate = new DateTime(2012, 5, 25);
+
+        public BookService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Book> GetBook()
+        {
+            return await _context.Books
+                .OrderByDescending(b => b.Price * b.QuantityPublished)
+                .FirstOrDefaultAsync();
+        }
+
+        public async Task<List<Book>> GetBooks()
+        {
+            var booksWithRedAfterSabaton = await _context.Books
+                .Where(b => b.Title.Contains("Red") && b.PublishDate > CarolusRexSabatonDate)
+                .ToListAsync();
+
+            return booksWithRedAfterSabaton;
+        }
+    }
+}
